86f06644be008df13acf5d9dde74d914a8df7288
shomrkm <shochan1985@gmail.com>
2020-08-23 18:11:42 +0900

    Add Developer class and Manager Class

--- oop_study.py
+++ oop_study.py
@@ -36,12 +36,50 @@ class Employee:
         return day.weekday() != 5 and day.weekday() != 6
 
 
-def main():
-    emp_str_1 = 'Shotaro-Murakami-60000'
-    emp_1 = Employee.from_string(emp_str_1)
+class Developer(Employee):
+    raise_amt = 1.10
 
-    my_date = datetime.date(2020, 8, 23)
-    print(Employee.is_workday(my_date))
+    def __init__(self, first, last ,pay, prog_lang):
+        super().__init__(first, last, pay)
+        self.prog_lang = prog_lang
+
+class Manager(Employee):
+    raise_amt = 1.10
+
+    def __init__(self, first, last ,pay, employees=None):
+        super().__init__(first, last, pay)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+
+    def add_emp(self, emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+
+    def remove_emp(self, emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+
+    def print_emps(self):
+        for emp in self.employees:
+            print('-->', emp.fullname())
+
+
+
+def main():
+    dev_1 = Developer('Shotaro', 'Murakami', 50000, 'Python')
+    dev_2 = Developer('Test', 'Employee', 55000, 'C++')
+
+    mgr_1 = Manager('Sue', 'Smith', 90000, [dev_1])
+    mgr_1.print_emps()
+    print('---')
+    mgr_1.add_emp(dev_2)
+    mgr_1.print_emps()
+    print('---')
+    mgr_1.remove_emp(dev_1)
+    mgr_1.print_emps()
+
 
 
 if __name__ == '__main__':
